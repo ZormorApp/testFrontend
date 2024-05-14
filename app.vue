0dc5250d25cd27b39ac8a29d73eec0de5d896759
DokuaAsiedu <asiedudokua@gmail.com>
2024-05-14 18:06:04 +0000

    created and added a navbar component and added to app.vue

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <NuxtLayout>
     <div className="container">
+      <navbar/>
       <NuxtPage/>
     </div>
   </NuxtLayout>
---
+++ components/navbar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div className="flex items-center justify-center">
+    <NuxtLink>
+      <NuxtImg height="8" src="/logo.svg"/>
+    </NuxtLink>
+  </div>
+</template>
