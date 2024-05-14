03c5a5ecaaba078d3dc3a5cb77a9f928278fbeab
DokuaAsiedu <asiedudokua@gmail.com>
2024-05-14 15:31:08 +0000

    added tailwind configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  alias: {
+    "@": "./"
+  },
   devServer: {
     port: 3100
+  },
+  css: [
+    "./assets/css/main.css"
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
   }
 })
