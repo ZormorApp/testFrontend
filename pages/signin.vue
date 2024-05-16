1616bae1261be332f674da06c5f364c02b21e2c6
hasppydays <jonbartplange@gmail.com>
2024-05-16 11:47:03 +0000

    signin and signup "UI" pages done.

---
+++ pages/signin.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container  flex justify-center items-center h-screen">
+        <div class="card">
+            <h2 class="text-center uppercase font-bold" >sign in</h2>
+            <form>
+                <label class="uppercase font-bold" >Email</label>
+                <input class="input" type="email" placeholder="enter your email">
+                <label class="uppercase font-bold">password</label>
+                <input class="input"  type="password" placeholder="enter your password">
+                <button type="submit" class="btn hover:bg-orange-400 font-bold">Sign in</button>
+                <p class="text-center">Don't have an account? <span><NuxtLink to="/signup" class="text-orange-500 hover:text-orange-300 ">Sign up</NuxtLink></span></p>
+            </form>
+        </div>
+    </div>
+  </template>
+  
+  <script setup>
+</script>
+  
+  <style scoped></style>
---
+++ pages/signup.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container  flex justify-center items-center h-screen">
+        <div class="card">
+            <h2 class="text-center uppercase font-bold" >sign up</h2>
+            <form>
+                <label class="uppercase font-bold" >Email</label>
+                <input class="input" type="email" placeholder="enter your email">
+                <label class="uppercase font-bold">password</label>
+                <input class="input"  type="password" placeholder="enter your password">
+                <button type="submit" class="btn hover:bg-orange-400 font-bold">Sign up</button>
+                <p class="text-center">Already have an account? <span><NuxtLink to="/signin" class="text-orange-500 hover:text-orange-300">Sign in</NuxtLink></span></p>
+            </form>
+        </div>
+    </div>
+  </template>
+  
+  <script setup>
+</script>
+  
+  <style scoped></style>
