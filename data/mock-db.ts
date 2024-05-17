30c3bb222b941867c3cd347c1e916577299012f6
DokuaAsiedu <asiedudokua@gmail.com>
2024-05-17 12:43:48 +0000

    created a mock database

---
+++ data/mock-db.ts
@@ -0,0 +1,152 @@
+export const PLACES_DB = [
+  {
+    id: 1,
+    name: "efficitur",
+    description:
+      "Suspendisse dignissim tincidunt odio sed tempus. Fusce at nunc orci. Donec tempor tempus consectetur. Curabitur ullamcorper orci a eros ultrices vestibulum. Integer mollis odio efficitur, finibus magna et, sollicitudin lorem. Mauris interdum vitae quam ac ornare. Sed tellus est, iaculis quis justo vitae, fringilla consectetur nibh. Ut eleifend vehicula tempus. In gravida dolor dolor, id volutpat dui condimentum eu. Quisque iaculis lobortis porta. Donec tincidunt fringilla pretium. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam sit amet efficitur nunc. Nulla efficitur lectus fermentum diam auctor vestibulum.",
+    location: "4053 7th Street",
+    openPeriods: [
+      {
+        days: ["mon", "tue", "wed", "thur", "fri"],
+        start: "08:00",
+        end: "14:00",
+      },
+    ],
+    images: [],
+  },
+  {
+    id: 2,
+    name: "Nam",
+    description:
+      "Sed ac commodo ligula. Morbi suscipit mi eget tortor euismod, a congue nibh finibus. Ut dapibus ut odio sit amet vehicula. Nulla congue posuere ligula ac pulvinar. Suspendisse potenti. Nulla libero odio, suscipit ut mattis non, tempor molestie diam. Nam sed ex mollis, luctus nisi at, rhoncus sapien. Integer eu ligula metus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+    location: "894 Riverside Drive",
+    openPeriods: [
+      {
+        days: ["mon", "tue", "wed", "thur", "fri"],
+        start: "08:00",
+        end: "14:00",
+      },
+    ],
+    images: [],
+  },
+  {
+    id: 3,
+    name: "sed",
+    description:
+      "Nulla orci mi, dapibus vel tempor vulputate, luctus at quam. Curabitur tempus sed mi eget pretium. Sed vitae eleifend nibh. Integer luctus orci vel metus elementum, sit amet rutrum nisi cursus. Cras sagittis eu magna et bibendum. Aliquam erat volutpat. Pellentesque sollicitudin ornare egestas. Fusce libero quam, elementum non auctor ultricies, vulputate non sem. Integer tristique est a lorem consectetur tempor.",
+    location: "6527 Magnolia Drive",
+    openPeriods: [
+      {
+        days: ["mon", "tue", "wed", "thur", "fri"],
+        start: "08:00",
+        end: "14:00",
+      },
+    ],
+    images: [],
+  },
+  {
+    id: 4,
+    name: "est",
+    description:
+      "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse luctus luctus metus et eleifend. Donec pharetra sem congue elementum venenatis. Nulla vitae odio urna. Ut sit amet eros orci. Praesent dictum ac dui a efficitur. Ut non hendrerit velit, nec mattis dolor. Ut venenatis elit quam, eget pulvinar augue finibus sed. Proin mi magna, venenatis eu sagittis vel, porttitor vel velit. Pellentesque nisl lacus, aliquam quis turpis ac, congue mollis arcu. Suspendisse eu nunc consequat, tincidunt diam eget, ornare diam. Curabitur iaculis vitae erat in eleifend.",
+    location: "39 Hill Street",
+    openPeriods: [
+      {
+        days: ["mon", "tue", "wed", "thur", "fri"],
+        start: "08:00",
+        end: "14:00",
+      },
+    ],
+    images: [],
+  },
+  {
+    id: 5,
+    name: "taciti",
+    description:
+      "Praesent nec euismod ex. Cras pharetra auctor imperdiet. Maecenas nisi ipsum, tristique eu metus eu, tincidunt accumsan nibh. Aenean et purus faucibus, fermentum risus eget, posuere diam. Fusce eget orci non sapien gravida sagittis. Curabitur placerat condimentum tortor. Duis cursus feugiat sem, quis convallis mi lobortis vitae. Praesent in dui mi. Ut egestas consectetur massa, vitae vulputate diam feugiat vitae. Nam tincidunt dui massa, et lacinia arcu cursus a. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris eu malesuada elit. Praesent accumsan venenatis leo eget tempor. Quisque luctus consequat faucibus.",
+    location: "993 9th Street",
+    openPeriods: [
+      {
+        days: ["mon", "tue", "wed", "thur", "fri"],
+        start: "08:00",
+        end: "14:00",
+      },
+    ],
+    images: [],
+  },
+  {
+    id: 6,
+    name: "purus",
+    description:
+      "Suspendisse ac nibh lacinia, maximus magna id, auctor erat. Proin auctor ultrices erat, id blandit arcu maximus vitae. Phasellus eu urna sapien. Vestibulum ornare arcu est. Integer viverra venenatis mauris, a pharetra dolor accumsan a. Nunc porta egestas arcu. Nunc quis odio id augue scelerisque finibus. Donec ultrices in felis in tincidunt. Integer pharetra massa at aliquet vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet justo eu tortor vehicula tincidunt. Vestibulum commodo, purus sit amet sollicitudin tincidunt, eros nibh lobortis massa, at blandit lectus turpis quis nulla. Proin accumsan tortor euismod tincidunt fringilla. Nunc a augue ante. Proin posuere dolor non turpis mattis varius. Suspendisse porttitor ornare rhoncus.",
+    location: "355 Lincoln Avenue",
+    openPeriods: [
+      {
+        days: ["mon", "tue", "wed", "thur", "fri"],
+        start: "08:00",
+        end: "14:00",
+      },
+    ],
+    images: [],
+  },
+  {
+    id: 7,
+    name: "sit",
+    description:
+      "Ut venenatis, tortor a commodo tincidunt, metus urna finibus lorem, ut pulvinar diam risus quis sapien. Nunc sit amet consectetur tortor. Suspendisse potenti. Nulla elementum hendrerit commodo. Sed suscipit et mi nec lobortis. Fusce efficitur metus ut orci ullamcorper suscipit. Sed condimentum sapien eu bibendum porta. Duis vel nunc quis leo congue interdum.",
+    location: "16 8th Street",
+    openPeriods: [
+      {
+        days: ["mon", "tue", "wed", "thur", "fri"],
+        start: "08:00",
+        end: "14:00",
+      },
+    ],
+    images: [],
+  },
+  {
+    id: 8,
+    name: "mi",
+    description:
+      "Cras metus ipsum, viverra vel ligula in, finibus eleifend odio. Morbi odio eros, euismod eget sapien non, placerat vehicula ex. Ut non nisi felis. In scelerisque tincidunt erat vel finibus. Vestibulum ut aliquam orci. Duis blandit vel lacus eget aliquet. Donec lacinia, elit sed tincidunt dictum, mi nisl cursus arcu, et fermentum diam sem eget purus. Nunc nec felis leo. Vivamus dictum lorem id lacus convallis lacinia quis nec quam. Maecenas faucibus sed nisi eu molestie. Fusce elementum ultrices risus, quis porttitor dolor porttitor nec. Duis convallis ante ipsum, id suscipit sem ultricies quis. Sed vel rhoncus tellus. Ut sed tincidunt nibh, non cursus nunc.",
+    location: "233 Central Avenue",
+    openPeriods: [
+      {
+        days: ["mon", "tue", "wed", "thur", "fri"],
+        start: "08:00",
+        end: "14:00",
+      },
+    ],
+    images: [],
+  },
+  {
+    id: 9,
+    name: "himenaeos",
+    description:
+      "Duis sodales dignissim leo vitae convallis. Praesent rhoncus pulvinar lectus quis suscipit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc ut elementum neque. Cras est ipsum, molestie vel vulputate vulputate, iaculis ac tellus. Nulla consectetur non ante et auctor. Etiam sodales lectus vel lectus scelerisque pretium ut sed felis. Maecenas congue felis vitae sapien dignissim rutrum. Proin in mi ex.",
+    location: "3847 2nd Avenue",
+    openPeriods: [
+      {
+        days: ["mon", "tue", "wed", "thur", "fri"],
+        start: "08:00",
+        end: "14:00",
+      },
+    ],
+    images: [],
+  },
+  {
+    id: 10,
+    name: "arcu",
+    description:
+      "Praesent nec commodo mauris, ut cursus odio. Donec venenatis tincidunt eros nec volutpat. Nulla convallis egestas finibus. Proin tellus nunc, cursus quis massa a, congue dictum massa. Phasellus tincidunt tristique placerat. Proin imperdiet feugiat consectetur. Nullam rhoncus ullamcorper libero quis viverra.",
+    location: "16573 7th Street",
+    openPeriods: [
+      {
+        days: ["mon", "tue", "wed", "thur", "fri"],
+        start: "08:00",
+        end: "14:00",
+      },
+    ],
+    images: [],
+  },
+];
