d8617508e30b123f579c965b2a235140aa712b75
DokuaAsiedu <asiedudokua@gmail.com>
2024-05-17 13:21:40 +0000

    style changes

--- components/landing.vue
+++ components/landing.vue
@@ -1,11 +1,7 @@
 <template>
   <section class="flex-1 flex justify-center items-center home bg-gray-900 bg-opacity-50">
     <div
-<<<<<<< HEAD
-      class="bg-gray-900 bg-opacity-50 w-screen h-full flex justify-center items-center">
-=======
       class="w-screen flex justify-center items-center">
->>>>>>> 3ed8039 (style change)
       <div class="text-center cursor-pointer">
         <h1 class="text-6xl font-bold uppercase font-sans text-orange-500">
           welcome to Zormor
