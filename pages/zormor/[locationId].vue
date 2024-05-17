58b5c3d4147459e559b10b8fe8e9de1a82ca3c55
DokuaAsiedu <asiedudokua@gmail.com>
2024-05-17 15:45:38 +0000

    added place specific page to be directed to when user clicks location card on list of places page

---
+++ pages/zormor/[locationId].vue
@@ -0,0 +1,36 @@
+<script setup>
+const route = useRoute()
+const placesStore = usePlacesStore()
+
+const { locationId } = route.params
+
+const place = placesStore.allPlaces.find(item => item.id === Number(locationId))
+
+console.log(typeof locationId, place)
+</script>
+
+<template>
+  <div class="flex-1 container py-4">
+    <div v-if="place" class="flex flex-col gap-2">
+      <div class="grid grid-cols-1 md:grid-cols-2 md:items-center">
+        <p class="capitalize font-bold text-3xl">{{place.name}}</p>
+
+        <p class="md:justify-self-end capitalize font-bold">{{place.location}}</p>
+      </div>
+
+      <div v-for="period in place.openPeriods" class="grid grid-cols-2 font-bold">
+        <p class="capitalize">{{period.days.join(', ')}}</p>
+
+        <p class="justify-self-end self-center">{{`${period.start} - ${period.end}`}}</p>
+      </div>
+
+      <p>{{place.description}}</p>
+    </div>
+
+    <div v-else class="h-full flex flex-col justify-center items-center gap-4 text-center text-xl">
+      <p class="text-5xl">Error</p>
+
+      <p>Sorry the page you're looking for doesn't exist</p>
+    </div>
+  </div>
+</template>
--- pages/zormor/index.vue
+++ pages/zormor/index.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 const placesStore = usePlacesStore()
 
-// callOnce(placesStore.fetch)
-console.log(placesStore.allPlaces)
-
 const data = placesStore.allPlaces
+
+const redirect = async (id: number) => {
+  await navigateTo(`/zormor/${id}`)
+}
 </script>
 
 <template>
   <div class="flex-1 py-4 container h-auto min-h-screen grid gap-4 text-white">
     <div
       v-for="item in data"
-      class="p-4 border-[1px] border-amber-300 rounded-lg hover:shadow-md hover:shadow-amber-200 hover:cursor-pointer">
+      class="p-4 border-[1px] border-amber-300 rounded-lg hover:shadow-md hover:shadow-amber-200 hover:cursor-pointer" @click="redirect(item.id)">
       <div class="grid sm:grid-cols-2">
         <p class="capitalize text-lg font-bold">{{item.name}}</p>
 
