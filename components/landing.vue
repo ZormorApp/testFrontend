33ce38e5d0c836965aac27e148b90d831f34d40b
DokuaAsiedu <asiedudokua@gmail.com>
2024-05-17 12:43:15 +0000

    style change

--- components/landing.vue
+++ components/landing.vue
@@ -1,7 +1,11 @@
 <template>
-  <section class="flex-1 flex justify-center items-center home">
+  <section class="flex-1 flex justify-center items-center home bg-gray-900 bg-opacity-50">
     <div
+<<<<<<< HEAD
       class="bg-gray-900 bg-opacity-50 w-screen h-full flex justify-center items-center">
+=======
+      class="w-screen flex justify-center items-center">
+>>>>>>> 3ed8039 (style change)
       <div class="text-center cursor-pointer">
         <h1 class="text-6xl font-bold uppercase font-sans text-orange-500">
           welcome to Zormor
@@ -14,6 +18,12 @@
           to="/about"
           class="btn2 inline-block hover:bg-orange-400 m-2 font-bold uppercase"
           >about us</NuxtLink
+        >
+
+        <NuxtLink
+          to="/all-places"
+          class="btn2 inline-block hover:bg-orange-400 m-2 font-bold uppercase"
+          >Browse Places</NuxtLink
         >
       </div>
     </div>
