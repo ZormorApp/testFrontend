3bf0c3d502ac4eab0ac7c760b089ff284b22d998
DokuaAsiedu <asiedudokua@gmail.com>
2024-05-19 16:45:55 +0000

    added openPeriods logic

--- pages/zormor/admin.vue
+++ pages/zormor/admin.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex-1 container flex justify-center items-center h-screen">
-    <div class="card">
+  <div class="container py-4 flex justify-center items-center">
+    <div class="">
       <h2 class="text-center uppercase font-bold">Create post</h2>
       <form>
         <label class="uppercase font-bold">Name</label>
@@ -12,12 +12,49 @@
           placeholder="enter place description" />
         <label class="uppercase font-bold">Location</label>
         <input class="input" type="text" placeholder="enter place location" />
-        <label class="uppercase font-bold">Opening time</label>
+        <!-- <label class="uppercase font-bold">Opening time</label>
         <input class="input" type="time" placeholder="enter opening time" />
         <label class="uppercase font-bold">Closing time</label>
-        <input class="input" type="time" placeholder="enter closing time" />
+        <input class="input" type="time" placeholder="enter closing time" /> -->
         <label class="uppercase font-bold">Image</label>
         <input class="input" type="text" placeholder="enter place location" />
+
+        <div class="mb-4 flex flex-col gap-3">
+          <p class="uppercase font-bold">Open Hours:</p>
+
+          <div class="flex flex-wrap gap-x-4 gap-y-2">
+            <button v-for="item in DAYS_OF_WEEK" @click="() => handleDays(item)" :class="`w-12 p-2 aspect-square grid place-items-center rounded-full border-[1px] border-chrome-yellow hover:bg-chrome-yellow/30 capitalize text-sm ${usedDays.includes(item) ? 'bg-chrome-yellow text-black' : ''}`" type="button">
+              <span>{{item}}</span>
+            </button>
+          </div>
+
+          <div v-for="(item, index) in openPeriods" class="py-2 flex justify-between gap-2 border-b-[1px] border-chrome-yellow">
+            <div class="flex-1 grid md:grid-cols-3 items-center gap-2">
+              <div class="grid grid-cols-2 gap-2">
+                <label for="start">Start:</label>
+                <input type="time" @input="(e) => handlePeriod(e.target.value, 'start')" :value="item.start" class="bg-transparent outline-none text-white"/>
+              </div>
+
+              <div class="grid grid-cols-2 gap-2">
+                <label for="start">End:</label>
+                <input type="time" @input="(e) => handlePeriod(e.target.value, 'end')" :value="item.end" class="bg-transparent outline-none text-white"/>
+              </div>
+
+              <div class="capitalize">
+                {{item.days.join(", ")}}
+              </div>
+            </div>
+
+            <button v-if="openPeriods.length > 1" class="p-2 grid place-items-center" type="button" @click="() => deletePeriod(index)">
+              <img src="~/assets/images/trash-can.svg" width="16" alt="delete button" />
+            </button>
+          </div>
+
+          <button type="button" :class="`p-2 w-max ${canAddPeriod} ? '' : 'hidden'`" @click="addNewPeriod">
+            <img src="~/assets/images/add.svg" width="16"/>
+          </button>
+        </div>
+
         <button type="submit" class="btn hover:bg-orange-400 font-bold">
           Create
         </button>
@@ -31,6 +68,81 @@ useHead({
   title: "zormor | create",
   meta: [{ name: "description", content: "zormor creation screen " }],
 })
+
+const DAYS_OF_WEEK = ["mon", "tue", "wed", "thur", "fri", "sat", "sun"];
+
+const openPeriods = ref([
+  {days: [], start: '', end: ''}
+])
+
+const pointer = ref(0)
+
+const usedDays = ref(openPeriods.value.map(item => item.days).flat(1))
+const canAddPeriod = ref(true)
+
+const handleDays = (day) => {
+  // get all days currently in openPeriods
+  // const usedDays = openPeriods.value.map(item => item.days).flat(1)
+  if (!usedDays.value.includes(day)) {
+    // if the selected day is not already in use, add it to the current days
+    openPeriods.value[pointer.value].days.push(day)
+    usedDays.value.push(day)
+  } else if (openPeriods.value[pointer.value].days.includes(day)) {
+    // if the selected day is part of the currently active period, remove it
+    const indexInPeriods = openPeriods.value[pointer.value].days.indexOf(day)
+    const indexInUsedDays = usedDays.value.indexOf(day)
+
+    openPeriods.value[pointer.value].days.splice(indexInPeriods, 1)
+    usedDays.value.splice(indexInUsedDays, 1)
+    // canAddPeriod = true
+  }
+}
+
+const handlePeriod = (value, type) => {
+  if (type === 'start') {
+    openPeriods.value[pointer.value].start = value
+  } else if (type === 'end') {
+    openPeriods.value[pointer.value].end = value
+  }
+}
+
+const addNewPeriod = () => {
+  // cannot add more than 7
+  if (openPeriods.value.length >= 7) {
+    canAddPeriod.value = false
+    return
+  }
+  // cannot add if previous is not filled
+  // const allValues = Object.values(openPeriods.value[pointer.value])
+  const prevStart = openPeriods.value[pointer.value].start
+  const prevEnd = openPeriods.value[pointer.value].end
+  const prevDays = openPeriods.value[pointer.value].days
+  if (!prevStart || !prevEnd || prevDays.length === 0) {
+    canAddPeriod.value = false
+    return
+  }
+  // cannot add if all days used up
+  if (DAYS_OF_WEEK.every(item => usedDays.value.includes(item))) {
+    canAddPeriod.value = false
+    return
+  }
+  // adding should increase pointer
+  canAddPeriod.value = true
+  pointer.value += 1
+  openPeriods.value.push({days: [], start: '', end: ''})
+}
+
+const deletePeriod = (index) => {
+  // only delete if openPeriods length is greater than 1
+  if (openPeriods.value.length > 1) {
+    openPeriods.value.splice(index, 1)
+    pointer.value -= 1
+    canAddPeriod.value = true
+  }
+}
+
+// const openPeriodsErrs = ref(" s")
+
 </script>
 
 <style scoped></style>
