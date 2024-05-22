875acc8082b821f9a9a7d3947cec91a2646d4d5f
hasppydays <jonbartplange@gmail.com>
2024-05-22 09:00:36 +0000

    about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="flex-1 container p-20">about page here</div>
+  <div class="flex-1 container p-10">
+    <h1 class="text-center uppercase font-bold text-2xl" >About page</h1>
+    <div class="flex justify-between p-10 items-center">
+      <div class="w-1/2 basis-1/2">
+        <h2 class="uppercase font-bold w-1/2 basis-1/2 "> about us</h2>
+        <p class="mr-4 ">Welcome to Zormor, 
+          the gateway to your dream destinations! 
+          With a passion for travel and a commitment to customer satisfaction,
+           we've been crafting unforgettable tours and experiences for adventurers
+            and leisure seekers alike. 
+            Our team of travel experts brings a wealth of knowledge
+            and a personal touch to every itinerary,
+            ensuring a journey that's as seamless as it is memorable.
+            From exotic locales to iconic landmarks,
+            we offer a diverse range of packages that cater to all tastes and budgets.
+            At Zormor, your next great story awaits. Let us take you there.</p>
+      </div>
+      <div class=" w-1/2 basis-1/2 ">
+        <NuxtImg src="/logo.svg" height="400" width="700"  />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
